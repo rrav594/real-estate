abb35321d448a8e1e2bebe522f34dacf1fbfa3bb
Ravi Ranjan <rav594.r@gmail.com>
2024-02-15 09:06:48 +0530

    create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="bg-zinc-300 shadow-lg">
+      <div className="flex mx-auto p-3 justify-between items-center max-w-6xl">
+        <Link to="/About">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-zinc-500">Real</span>
+            <span className="text-zinc-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="flex items-center bg-zinc-200 p-3 rounded-lg">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch />
+        </form>
+        <ul className="flex gap-6">
+          <Link to="/">
+            <li className="hover:cursor-pointer hidden sm:inline text-zinc-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/About">
+            <li className="hover:cursor-pointer hidden sm:inline text-zinc-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="sign-in">
+            <li className="hover:cursor-pointer sm:inline text-zinc-700 hover:underline">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
