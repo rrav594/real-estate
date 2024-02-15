bd5ff9d6b6423cada488ec22611c277d74e0822b
Ravi Ranjan <rav594.r@gmail.com>
2024-02-15 09:28:36 +0530

    connect to mongoDB database using mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 dotenv.config({ path: "./config.env" });
 
 const app = express();
+
+// connect to database
+mongoose
+  .connect(process.env.DB_URI)
+  .then(console.log("Connection established to database..."))
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 // start listening
 const port = process.env.PORT || 3000;
