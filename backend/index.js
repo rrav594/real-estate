fcaed7452951ab39a939c30893c81097df0c0ee8
Ravi Ranjan <rav594.r@gmail.com>
2024-02-15 09:17:00 +0530

    create and run express server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 import dotenv from "dotenv";
+import express from "express";
 
-dotenv.config({ path: "../config.env" });
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+
+// start listening
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server start on port ${port}`);
+});
