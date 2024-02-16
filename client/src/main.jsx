4b3400c720872fa31474808fbe8c1ae25dbe2756
Ravi Ranjan <rav594.r@gmail.com>
2024-02-16 12:52:43 +0530

    add react-persist to store the currentUser

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./store/store.js";
+import { store, persistor } from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
