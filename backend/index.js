0d20d1094b212b38d9bf6e2d96568a40e53106e7
Ravi Ranjan <rav594.r@gmail.com>
2024-03-08 17:03:14 +0530

    add pre installation

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
-appendFile.get("*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
 });
 
